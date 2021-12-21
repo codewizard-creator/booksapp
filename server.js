1b88705ed50f3d38d25bc173c90a366c9542db7c
Oguzhan <ahmetoguzhany@gmail.com>
2021-12-21 19:30:11 +0300

    error disabled

--- server.js
+++ server.js
@@ -45,4 +45,4 @@ if(process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => 
-    console.log(`Server is running on port ${port}`));
+    console.log(`Server is running on port ${PORT}`));
